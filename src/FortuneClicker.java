cef563badfea7e221cb750cc90fc5d394ed34c74
NIO_Path_File <gopinasi@mail.uc.edu>
2024-09-27 18:44:22 -0400

    Final Commit

--- src/FortuneClicker.java
+++
@@ -1,9 +0,0 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class FortuneClicker implements ActionListener {
-    public void actionPerformed(ActionEvent e) {
-        System.out.println("Fortune");
-    }
-
-}
